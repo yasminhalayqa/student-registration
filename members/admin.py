d409645ed3dc1efe18caf906375b364c11cb2798
jasmin <yasmeenhal2002@gmail.com>
2024-05-26 12:28:38 +0300

    create admin views(add course, add course schedule)

--- members/admin.py
+++ members/admin.py
@@ -1,3 +1,15 @@
+# admin.py
 from django.contrib import admin
+from .models import Courses, courseSchedule
 
-# Register your models here.
+class CoursesAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name', 'instructor', 'capacity', 'students_enrolled', 'registration_deadline')
+
+
+class courseScheduleAdmin(admin.ModelAdmin):
+    list_display = ('days', 'startTime', 'endTime', 'roomNo')
+
+
+
+admin.site.register(Courses, CoursesAdmin)
+admin.site.register(courseSchedule, courseScheduleAdmin)
